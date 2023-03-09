7cbaecfdd0e3b09bc17b1438f262c35c6319b6c5
verba05 <verba447@gmail.com>
2023-03-09 13:07:48 +0100

    work

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import com.something.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Point c = new Point(2,-2);
+        Point d = new Point(2,-6);
+        Segment a = new Segment(c,d);
+        Segment b = new Segment(new Point(), new Point(1,0));
+    }
+}
---
+++ src/com/something/Point.java
@@ -0,0 +1,7 @@
+package com.something;
+
+public class Point {
+    public double x, y;
+    public Point(){this.x = 0; this.y = 0;}
+    public Point(double x, double y){this.x = x; this.y = y;}
+}
