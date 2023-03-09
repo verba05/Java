da0b4aa403574f9404ed4b236a0eeee22f990e6d
verba05 <verba447@gmail.com>
2023-03-09 16:07:12 +0100

    commit

--- src/com/something/Polygon.java
+++ src/com/something/Polygon.java
@@ -22,4 +22,9 @@ public class Polygon {
         };
         return String.format(Locale.ENGLISH,"<polygon points=\"%s\" style=\"%s\"/>",s, style.toSvg());
     }
+    public static Polygon square(Segment segment, Style style){
+        Point p = new Point((segment.p1.x + segment.p2.x) / 2,(segment.p1.y + segment.p2.y) / 2);
+        Segment segments[] = Segment.perpendicular(segment, p, segment.length()/2);
+
+    }
 }
--- src/com/something/Segment.java
+++ src/com/something/Segment.java
@@ -24,4 +24,19 @@ public class Segment {
         String str = ("<line x1=\"" + p1.x + "\" y1=\"" + p1.y + "\" x2=\"" + p2.x + "\" y2=\"" + p2.y + "\" stroke=\"black\" stroke-width=\"2\" />\n");
         return str;
     }
+    static public Segment[] perpendicular(Segment segment, Point point){
+        double a, b, x0, y0, x1, y1, x2, y2, sq, r;
+        a = (segment.p1.y - segment.p2.y) / (segment.p1.x - segment.p2.x);
+        b = point.y - a * point.x;
+        x0 = point.x;
+        y0 = point.y;
+        r = segment.length();
+        sq = Math.sqrt(-(y0*y0)+(2*a*x0+2*b)*y0-(a*a)*(x0*x0)-2*a*b*x0+(a*a+1)*r*r-b*b);
+        x1 = (-(sq)-a*y0-x0+a*b)/(a*a+1);
+        y1 = -(a*sq+a*a*y0+a*x0+b)/(a*a+1);
+        x2 = (sq+a*y0+x0-a*b)/(a*a+1);
+        y2 = (a*sq+a*a*y0+a*x0+b)/(a*a+1);
+        return new Segment[]{new Segment(point,new Point(x1,y1)),new Segment(point,new Point(x2,y2))};
+
+    }
 }
